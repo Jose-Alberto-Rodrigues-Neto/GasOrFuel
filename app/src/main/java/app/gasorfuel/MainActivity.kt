f6cd8d72a28ba8b081601ad525f7e22be9e83953
Alberto Rodrigues <allio.professional@gmail.com>
2024-12-04 17:44:27 -0300

    feat: creating new components

--- app/src/main/java/app/gasorfuel/MainActivity.kt
+++ app/src/main/java/app/gasorfuel/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import app.gasorfuel.ui.theme.GasOrFuelTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +31,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             GasOrFuelTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    MainPage(
+                        modifier = Modifier
+                            .padding(innerPadding)
                     )
                 }
             }
@@ -30,18 +41,30 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainPage(modifier: Modifier){
+    var userInput by remember { mutableStateOf("") }
+    GasOrFuelTheme {
+        Column {
+            Text(
+                modifier = Modifier,
+                text = "Gas or Fuel?",
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold,
+            )
+            OutlinedTextField(value = "text", onValueChange = { userInput = it}, label = { Text("label") })
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    GasOrFuelTheme {
-        Greeting("Android")
+fun MainPagePreview() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        MainPage(
+            modifier = Modifier
+                .padding(innerPadding)
+        )
     }
 }
