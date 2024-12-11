9ae1de0709ee341101c4f3180dc2344ae0bc502d
Alberto Rodrigues <allio.professional@gmail.com>
2024-12-11 16:31:10 -0300

    stting up project

--- app/src/main/java/app/gasorfuel/MainActivity.kt
+++ app/src/main/java/app/gasorfuel/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -45,6 +46,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainPage(modifier: Modifier){
     var userInput by remember { mutableStateOf("") }
+    var isChecked by remember { mutableStateOf(false) }
     GasOrFuelTheme {
         Column {
             Text(
@@ -53,7 +55,15 @@ fun MainPage(modifier: Modifier){
                 fontSize = 28.sp,
                 fontWeight = FontWeight.Bold,
             )
-            OutlinedTextField(value = "text", onValueChange = { userInput = it}, label = { Text("label") })
+            OutlinedTextField(
+                value = userInput,
+                onValueChange = { userInput = it},
+                label = { Text("label") }
+            )
+            Switch(
+                checked = isChecked,
+                onCheckedChange = { isChecked = it }
+            )
         }
     }
 }
